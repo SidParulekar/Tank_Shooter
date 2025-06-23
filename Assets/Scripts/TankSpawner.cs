e9f6d62c8d3554d857f632aad8a37af688472339
SidParulekar <76064146+SidParulekar@users.noreply.github.com>
2025-06-23 15:18:25 +0530

    Created Tank Prefab and Tank Spawner C# Script

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject tankPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(tankPrefab, transform.position, Quaternion.identity); //Creating a tank in the game scene with a specified position and rotation
+    }
+
+}
