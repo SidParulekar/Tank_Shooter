0a6ee63708f840e582c17876c7003dc973689cce
SidParulekar <76064146+SidParulekar@users.noreply.github.com>
2025-06-26 14:56:18 +0530

    Implemented Tank Movement and Rotation
    Set camera to be child object of tank when Game Starts so it can follow the tank as it moves.
    
    Implemented receiving of movement input in TankView, logic of tank movement in TankController and setting of movementSpeed and rotationSpeed in TankModel.
    
    Defined function to access TankModel instance via TankController.
    
    TankView accesses movementSpeed and rotationSpeed in TankModel via TankController to call Move and Rotate functions defined in TankController.

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -7,14 +7,34 @@ public class TankController
     private TankModel tankModel;
     private TankView tankView;
 
+    private Rigidbody rb;
+
     public TankController(TankModel _tankModel, TankView _tankView)
     {
         tankModel = _tankModel;
-        tankView = _tankView;
+        tankView = GameObject.Instantiate<TankView>(_tankView); // Assigning the TankView component attached to Game Object being instantiated
 
         tankModel.SetTankController(this);
         tankView.SetTankController(this);
 
-        GameObject.Instantiate(tankView.gameObject);
+        rb = tankView.GetRigidbody();
+
+    }
+
+    public void Move(float movementDir, float movementSpeed)
+    {
+        rb.velocity = tankView.transform.forward * movementDir * movementSpeed;
+    }
+
+    public void Rotate(float rotateDir, float rotateSpeed)
+    {
+        Vector3 vector = new Vector3(0f, rotateDir * rotateSpeed, 0f); // specifying direction and speed of rotation
+        Quaternion deltaRotation = Quaternion.Euler(vector * Time.deltaTime);// additional variable for smooth rotation
+        rb.MoveRotation(rb.rotation *  deltaRotation); // implementing rotation in the rigidbody of tank so it is visible on the screen
+    }
+
+    public TankModel GetTankModel()
+    {
+        return tankModel;
     }
 }
--- Assets/Scripts/TankView.cs
+++ Assets/Scripts/TankView.cs
@@ -6,6 +6,11 @@ public class TankView : MonoBehaviour
 {
     private TankController tankController;
 
+    private float movementDir;
+    private float rotationDir;
+
+    [SerializeField] private Rigidbody rb;
+
     public void SetTankController(TankController tankController)
     {
         this.tankController = tankController;
@@ -14,12 +19,38 @@ public class TankView : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject cam = GameObject.Find("Main Camera");
+
+        cam.transform.SetParent(this.transform); // Defining the tank object to which this TankView script is attached as Parent of cam
+
+        cam.transform.position = new Vector3(0f, 11f, -5f); // Setting distance between the tank and camera
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovementInput();
+
+        if(movementDir!=0)
+        {
+            tankController.Move(movementDir, tankController.GetTankModel().movementSpeed);
+        }
+
+        if (rotationDir != 0)
+        {
+            tankController.Rotate(rotationDir, tankController.GetTankModel().rotationSpeed);
+        }
+    }
+
+    private void MovementInput()
+    {
+        movementDir = Input.GetAxis("Vertical");
+
+        rotationDir = Input.GetAxis("Horizontal");
+    }
+
+    public Rigidbody GetRigidbody()
+    {
+        return rb;
     }
 }
